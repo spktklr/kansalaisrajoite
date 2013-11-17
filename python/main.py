ece46d79a5f688f56443471e2cd3544b8a6bc7d3
Samu Nuutamo <samu.nuutamo@gmail.com>
2013-11-17 18:06:54 +0200

    argparse otettu käyttöön komentoriviparametrien parsimisessa

--- python/main.py
+++ python/main.py
@@ -2,14 +2,17 @@
 # coding=utf-8
 import root
 import bottle
-import sys
+import argparse
 
-def main(args):
-    debug = '--debug' in args
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--debug', help='enable debug mode and autoreload', action='store_true')
+	parser.add_argument('--host', help='set to 0.0.0.0 to listen on all interfaces', type=str, default='127.0.0.1')
+	parser.add_argument('--port', help='listen port', type=int, default=8080)
+	args = parser.parse_args()
 
-    bottle.debug(debug)
-    bottle.run(app=root.middleware_app, reloader=debug, host='0.0.0.0')
+	bottle.debug(args.debug)
+	bottle.run(app=root.middleware_app, reloader=args.debug, host=args.host, port=args.port)
 
 if __name__ == "__main__":
-    main(sys.argv)
-
+	main()
