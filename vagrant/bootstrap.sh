24d3779e36229f2e11aaa12df4f67c78634812ae
Samu Nuutamo <samu.nuutamo@gmail.com>
2013-11-18 22:30:33 +0200

    cleaned up requirements.txt, removed pycrypto dependency

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -7,7 +7,7 @@ apt-get update
 apt-get install -y git python-software-properties python-dev python-pip python-flup libffi-dev postgresql libpq-dev
 
 # Install python dependencies
-pip install bottle sqlalchemy bottle-sqlalchemy bcrypt beaker pycrypto
+pip install bottle sqlalchemy bottle-sqlalchemy bcrypt beaker
 pip install argparse pycparser wsgiref cffi psycopg2 --upgrade
 
 # Create a database user and the database, and import fixture contents
