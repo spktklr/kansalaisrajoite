bd9f1f033e9239ab8e67ab5cd5a9538ca92806b4
spktklr <nmyllynen@gmail.com>
2013-12-17 21:18:56 +0200

    ääkköset db:ssa kuntoon

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+﻿#!/bin/bash
 
 # Readies kansalaisrajoite server for development
 
@@ -13,6 +13,6 @@ pip install argparse pycparser wsgiref cffi psycopg2 --upgrade
 # Create a database user and the database, and import fixture contents
 cd /vagrant/kansalaisrajoite/db/
 sudo -u postgres createuser -E -d -r -S vagrant
-sudo -u vagrant createdb kansalaisrajoite
+sudo -u vagrant createdb kansalaisrajoite --lc-collate 'en_US.UTF-8' --lc-ctype 'en_US.UTF-8' --encoding 'UTF-8' --template 'template0'
 sudo -u vagrant psql -d kansalaisrajoite -f create.sql
 sudo -u vagrant psql -d kansalaisrajoite -f populate.sql
