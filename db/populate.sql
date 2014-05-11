a8c9f8d86dabce3bdac619050584483ec9393d97
spktklr <nmyllynen@gmail.com>
2014-05-11 15:31:40 +0300

    Ekan tiedotteen lisäys

--- db/populate.sql
+++ db/populate.sql
@@ -35,7 +35,16 @@ INSERT INTO vote (restriction_id, user_id) VALUES (6, 3);
 INSERT INTO vote (restriction_id, user_id) VALUES (6, 4);
 INSERT INTO vote (restriction_id, user_id) VALUES (6, 5);
 
-INSERT INTO news (title, body, user_id) VALUES ('Eka tiedote', 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.', 1);
-INSERT INTO news (title, body, user_id) VALUES ('Toka tiedote', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.', 2);
+INSERT INTO news (title, body, user_id) VALUES ('Kansalaisrajoite.fi-verkkopalvelu avataan 11. toukokuuta', 'Verkkopalvelu kansalaisrajoitteiden tekemiseksi ja rajoitteiden kannatuksen keräämiseksi avataan kansalaisten käyttöön sunnuntaina 11. toukokuuta. Palvelu toimii osoitteessa www.kansalaisrajoite.fi. Se tarjoaa kansalaisille mahdollisuuden saada rajoitteensa kannatettavaksi.
+
+Verkkopalvelu on kansalaisille ilmainen. Palvelun ylläpito tarkastaa, että jätetyissä rajoitteissa on tarvittavat tiedot ja ettei aloite sisällä verkossa julkaistavaksi sopimatonta materiaalia. Tämän jälkeen rajoitteelle voi kerätä kannatusta.
+
+Palvelu vaatii rajoitteiden luojilta ja kannattajilta rekisteröitymisen. Rajoite voi koskea mitä tahansa kansalaisen elämän osa-aluetta. Vähintään 101 kansalaisella on oikeus saada rajoite täysin kannatetuksi. Kannatuksen keräämiselle ei ole määräaikaa.
+
+Palveluun liittyviin kysymyksiin vastataan sähköpostitse sekä sosiaalisessa mediassa, Facebookissa.
+
+<a href="http://www.kansalaisrajoite.fi">http://www.kansalaisrajoite.fi</a> 
+
+<a href="https://www.facebook.com/kansalaisrajoite">https://www.facebook.com/kansalaisrajoite</a>', 1);
 
 COMMIT;
