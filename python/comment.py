e313c711a4c5a807341c3fc7f7caf27bfb474ba1
Pauli Kärpänoja <pauli.karpanoja@solita.fi>
2014-06-20 14:12:41 +0300

    Interface for listing comments to restriction

--- python/comment.py
+++ python/comment.py
@@ -1,4 +1,4 @@
-from bottle import Bottle, HTTPError
+from bottle import Bottle, HTTPError, request
 
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -11,12 +11,32 @@ app = Bottle()
 app.install(model.plugin)
 app.install(jsonplugin)
 
+@app.get('/<id:int>')
+def read_one(db, id):
+    try:
+        items = db.query(model.Comment) \
+            .filter_by(restriction_id=id).all()
+        return {"comments" : [i.toDict() for i in items]}
+    except NoResultFound:
+        return HTTPError(404, 'Not found')
+
 @app.post('/<id:int>')
 @auth.require_login
 def create(db, user, id):
     try:
-        print user
-        #item = db.query(model.Restriction).filter_by(id=id).one()
-        #item.voters.append(user)
+
+        # comment = model.Comment
+        #
+        # comment.restriction_id = id
+        # comment.user_id = user.id
+        # comment.comment = request.forms.comment
+        # #
+        # # items = db.query(model.Comment).filter_by(restriction_id=id).all()
+        # # print items
+        #
+        # item = db.query(model.Restriction).filter_by(id=id).one()
+        # print item
+        # print item.comments
+        # item.comments.append(comment)
     except NoResultFound:
         return HTTPError(404, 'Not found')
